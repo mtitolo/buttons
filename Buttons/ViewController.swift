045da9dcec8b8827bb6b8a7d896e9f764e446056
Michele <michele.titolo@gmail.com>
2015-06-09 09:48:53 -0700

    Initial Commit

---
+++ Buttons/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Buttons
+//
+//  Created by Michele Titolo on 6/9/15.
+//  Copyright © 2015 Michele Titolo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
